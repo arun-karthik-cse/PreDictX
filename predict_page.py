70767e7654ec787ebb4402ba041fe9ee6ebab919
arun-karthik-cse <arunkarthikeyanb.cse@gmail.com>
2024-12-14 22:56:01 +0530

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -197,8 +197,6 @@ st.markdown("""
 """, unsafe_allow_html=True)
 import pandas as pd
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 st.markdown("""
     <style>
